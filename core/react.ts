1ca3fa69cb95293d84f85d39b388e5c21a81e787
glows777 <1914426389@qq.com>
2024-01-14 19:44:26 +0800

    feat: simple fiber

--- core/react.ts
+++ core/react.ts
@@ -1,3 +1,5 @@
+import { Fiber, transformVdomToFiber } from './fiber'
+
 export type ElementType = string
 export type PropsType = Record<string, any> & { children: ChildType[] }
 export type ChildType = VDOMElement | string
@@ -31,29 +33,98 @@ export const createText = (text: string): VDOMElement => ({
 })
 
 export const render = (el: VDOMElement, container: HTMLElement): void => {
-  const { type, props } = el
-  // * 创建 dom
-  const dom: HTMLElement | Text =
-    type === TEXT_ELEMENT
-      ? document.createTextNode(props.nodeValue as string)
-      : document.createElement(type as string)
+  nextWorOfUnit = transformVdomToFiber(
+    {
+      type: '',
+      props: {
+        children: [el],
+      },
+    },
+    container
+  )
+  // 推入构造环节
+  requestIdleCallback(workLoop)
+}
 
-  // * 添加 props 属性
+let nextWorOfUnit: null | Fiber = null
+const workLoop: IdleRequestCallback = (deadline) => {
+  let shouldYield = false
+
+  while (!shouldYield && nextWorOfUnit) {
+    // * 一边构建链表，一边构造dom
+    nextWorOfUnit = performWorkOfUnit(nextWorOfUnit)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+const createDom = (type: ElementType) => {
+  return type === TEXT_ELEMENT
+    ? document.createTextNode('')
+    : document.createElement(type as string)
+}
+const updateProps = (props: PropsType, dom: HTMLElement | Text) => {
   Object.keys(props).forEach((k) => {
     if (k !== 'children') {
       ;(dom as any)[k] = props[k]
     }
   })
+}
 
-  // * 递归处理 children
-  props.children.forEach((child: ChildType) => {
-    // 是 字符串，则不需要递归处理了
-    if (typeof child === 'object') {
-      render(child, dom as HTMLElement)
+const bindFiberTree = (fiber: Fiber) => {
+  let prevChild: Fiber | null = null
+  fiber.props.children.forEach((child, index) => {
+    if (typeof child !== 'string') {
+      // * 创建新的 fiber 节点
+      const newFiber = new Fiber({
+        ...transformVdomToFiber(child, null),
+        parent: fiber,
+      })
+      // * 第一个节点，那么，父节点的 child 应该指向她
+      // * 否则， 则前序节点的 sibling 指向当前节点
+      if (index === 0) {
+        fiber.child = newFiber
+      } else {
+        prevChild!.sibling = newFiber
+      }
+      prevChild = newFiber
     }
   })
+}
 
-  container.appendChild(dom)
+const performWorkOfUnit = (fiber: Fiber) => {
+  const { type, props } = fiber
+
+  // * 1. 创建 dom & 处理 props
+  // * 如果 dom 存在，则代表是 入口container，不需要创建，开始处理子节点即可
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(type))
+    // @ts-expect-error Text does't have this method
+    fiber.parent?.dom?.append(dom)
+    // * 处理 props
+    updateProps(props, dom)
+  }
+  // * 2. 绑定 指针指向
+  bindFiberTree(fiber)
+
+  // * 3. 返回
+  // * 此时的优先级为
+  // *    1. 孩子节点
+  // *    2. 兄弟节点
+  // *    3. ”叔叔“ 节点， 也可以是 ”曾叔叔“, ......, ”祖先叔叔“
+  if (fiber.child) {
+    return fiber.child
+  }
+  let next: Fiber | null = fiber
+  while (next) {
+    // * 如果 sibling 存在，则返回sibling
+    if (next.sibling) {
+      return next.sibling
+    }
+    // * 否则，继续往上寻找 parent 的 sibling
+    next = next.parent
+  }
+  return null
 }
 
 const React = {
--- playgrouund/App.tsx
+++ playgrouund/App.tsx
@@ -1,5 +1,8 @@
 import React from '../core/react'
 
-const App = <div id="hello">hello m-react</div>
+const App = <div id="hello">hello m-react<h1>hello<h2>world</h2></h1></div>
+
+console.log(App);
+
 
 export default App
